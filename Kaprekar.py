803225a93fc1fb467300b22edf81a9c8afb9ae4d
Hugo <hfraile@iessarenal.net>
2025-10-28 13:05:24 +0100

    Kaprekar terminado

--- Kaprekar.py
+++ Kaprekar.py
@@ -1,23 +1,31 @@
-def keprekar(case:str, tries = 0)->int:
-    list_num = list(case)
-    list_num.sort()
-    a = list_num.copy()
-    list_num.sort(reverse=True)
-    b = list_num.copy()
-    a = int("".join(a))
-    b = int("".join(b))
-    if a > b:
-        res = a-b
-    else:
-        res = b-a
-    
-    print(res)
-    if res != 6174:
-        print(tries)
-        keprekar(str(res), tries=tries+1)
-    else:
-        print(tries)
-        return tries
+def keprekar(case:str)->int:
+    res = 0
+    tries = 0
+    while case != "6174":
+        list_num = list(case)
+        list_num.sort()
+        a = list_num.copy()
+        list_num.sort(reverse=True)
+        b = list_num.copy()
+        a = int("".join(a))
+        b = int("".join(b))
+        if a > b:
+            res = a-b
+        else:
+            res = b-a
+        if tries > 7:
+            break
+
+        if res != 6174:
+            tries += 1
+            case = str(res)
+            if len(case) < 4:
+                case += "0"*(4-len(case))
+        else:
+            tries += 1
+            break
+
+    return tries
     
 
 num = []
